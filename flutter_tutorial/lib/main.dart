7e5d398a199a911ed648b2fa90e37a1b35cf8a77
caots <namanhle941998@gmail.com>
2025-02-14 16:08:24 +0700

    Quote app

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,78 +1,53 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int ninjaLevel = 0;
+
+  List<Quote> quotes = [
+    Quote(
+        author: "Oscar Wilde",
+        text: "Be yourself; everyone else is already taken"),
+    Quote(
+        author: "Oscar Wilde",
+        text: "I have nothing to declare except my genius"),
+    Quote(
+        author: "Oscar Wilde",
+        text: "The truth is rarely pure and never simple")
+  ];
+
+  void handleDelete(quote) {
+    setState(() {
+      quotes.remove(quote);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Ninja ID Card", style: TextStyle(color: Colors.white60)),
+          title: Text("Quote List", style: TextStyle(color: Colors.white60)),
           centerTitle: true,
           backgroundColor: Colors.grey[800],
         ),
-        body: Container(
-            alignment: Alignment.topLeft,
-            color: Colors.grey[850],
-            padding: EdgeInsets.fromLTRB(30, 50, 30, 50),
-            child: Column(
-              children: [
-                Center(
-                  child: CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: AssetImage('assets/thumb.jpg'),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Name',
-                          style:
-                              TextStyle(fontSize: 20.0, color: Colors.white60)),
-                      Text('Chun-Li',
-                          style: TextStyle(
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.yellow[400])),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Current Ninja Level',
-                          style:
-                              TextStyle(fontSize: 20.0, color: Colors.white60)),
-                      Text('8',
-                          style: TextStyle(
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.yellow[400])),
-                    ],
-                  ),
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.mail, color: Colors.white60),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'ninja.example@gmail.com',
-                      style: TextStyle(color: Colors.white60, fontSize: 15.0),
-                    )
-                  ],
-                )
-              ],
-            )));
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: quotes
+                .map((quote) => QuoteCard(
+                    quote: quote,
+                    delete: () {
+                      handleDelete(quote);
+                    }))
+                .toList()));
   }
 }
